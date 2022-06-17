71c0e9e1a3c900637c7820ce6563baf62fa45253
ki-ljl <lijunliang.ki@gmail.com>
2022-06-17 23:04:27 +0800

    update seq2seq

--- models.py
+++ models.py
@@ -74,9 +74,8 @@ class Encoder(nn.Module):
         batch_size, seq_len = input_seq.shape[0], input_seq.shape[1]
         h_0 = torch.randn(self.num_directions * self.num_layers, batch_size, self.hidden_size).to(device)
         c_0 = torch.randn(self.num_directions * self.num_layers, batch_size, self.hidden_size).to(device)
-        # print(input_seq.size())
-        # output(batch_size, seq_len, num_directions * hidden_size)
         output, (h, c) = self.lstm(input_seq, (h_0, c_0))
+
         return h, c
 
 
@@ -93,20 +92,31 @@ class Decoder(nn.Module):
         self.linear = nn.Linear(self.hidden_size, self.output_size)
 
     def forward(self, input_seq, h, c):
-        batch_size, seq_len = input_seq.shape[0], input_seq.shape[1]
-        output, _ = self.lstm(input_seq, (h, c))
-        pred = self.linear(output)  # pred()
+        # input_seq(batch_size, input_size)
+        batch_size = input_seq.shape[0]
+        input_seq = input_seq.view(batch_size, 1, self.input_size)
+        output, (h, c) = self.lstm(input_seq, (h, c))
+        # output(batch_size, seq_len, num * hidden_size)
+        pred = self.linear(output)  # pred(batch_size, 1, output_size)
         pred = pred[:, -1, :]
-        return pred
+
+        return pred, h, c
 
 
 class Seq2Seq(nn.Module):
     def __init__(self, input_size, hidden_size, num_layers, output_size, batch_size):
         super().__init__()
+        self.output_size = output_size
         self.Encoder = Encoder(input_size, hidden_size, num_layers, batch_size)
         self.Decoder = Decoder(input_size, hidden_size, num_layers, output_size, batch_size)
 
     def forward(self, input_seq):
+        batch_size, seq_len, _ = input_seq.shape[0], input_seq.shape[1], input_seq.shape[2]
         h, c = self.Encoder(input_seq)
-        pred = self.Decoder(input_seq, h, c)
-        return pred
+        outputs = torch.zeros(batch_size, seq_len, self.output_size).to(device)
+        for t in range(seq_len):
+            _input = input_seq[:, t, :]
+            output, h, c = self.Decoder(_input, h, c)
+            outputs[:, t, :] = output
+
+        return outputs[:, -1, :]
